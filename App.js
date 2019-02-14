c6c92477e21d93586df52d40a8cb4fff5d5d0d49
stuart black <stoo@stuarts-MacBook-Pro.local>
2019-02-14 20:27:47 +0000

    Resolved props and states for header nut choice change

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Logo from './Logo';
+import Heading from './Components/Heading';
 
 export default class App extends React.Component {
 constructor() {
@@ -14,23 +15,25 @@ constructor() {
     return (
       <View style={styles.container}>
       <View style={styles.logo}><Text>Logo</Text></View>
-      <View style={styles.heading}><Text>{this.state.nutChoice}</Text></View>
+        <View style={styles.heading}>
+          <Heading title={this.state.nutChoice} />
+        </View>
       <View style={styles.nutchoice}>
         <Button 
           title="Pecan"
-          onPress={() => this.setState({ nutChoice: 'Pecan' + ' Milk'})}
+          onPress={() => this.setState({ nutChoice: 'Pecan'})}
         /> 
         <Button 
           title="Almond"
-          onPress={() => this.setState({ nutChoice: 'Almond' + ' Milk'})}
+          onPress={() => this.setState({ nutChoice: 'Almond'})}
         /> 
          <Button 
           title="Cashew"
-          onPress={() => this.setState({ nutChoice: 'Cashew' + ' Milk'})}
+          onPress={() => this.setState({ nutChoice: 'Cashew'})}
         /> 
          <Button 
           title="Pistachio"
-          onPress={() => this.setState({ nutChoice: 'Pistachio' + ' Milk'})}
+          onPress={() => this.setState({ nutChoice: 'Pistachio'})}
         /> 
       </View>
       <View style={styles.settings}><Text>Settings Here</Text></View>
---
+++ Components/Heading.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const heading = (props) => {
+    return <Text>{props.title} Milk </Text>
+}
+
+export default heading;
